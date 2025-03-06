6ba071cab501b5600fe53a9799e25aae01c51e33
Georgi Raev <georgihraev89@gmail.com>
2025-03-06 17:27:05 +0200

    Add drivers query-GetAllDriversQuery.cs and GetDriverByIdQuery.cs

---
+++ F1_Web_App/Aplication/Drivers/Queries/GetAllDriversQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using F1_Web_App.Models;
+using System.Collections.Generic;
+using F1_Web_App.Data.Models;
+
+public class GetAllDriversQuery : IRequest<IEnumerable<Driver>> { }
---
+++ F1_Web_App/Aplication/Drivers/Queries/GetDriverByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using F1_Web_App.Models;
+using F1_Web_App.Data.Models;
+
+public class GetDriverByIdQuery : IRequest<Driver?>
+{
+    public int Id { get; set; }
+
+    public GetDriverByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
